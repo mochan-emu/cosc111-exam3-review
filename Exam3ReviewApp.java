44593a71cce853db7d02e735a147ece1091a615d
Mochan Shrestha <mochan@emich>
2017-03-21 05:59:34 -0400

    Starter files.

---
+++ Exam3ReviewApp.java
@@ -0,0 +1,14 @@
+
+
+public class Exam3ReviewApp
+{
+    public static void main(String[] args)
+    {
+        // Shareholder names and shares
+        String[] shareholder_names = {"Adam", "Beth", "Chris", "Don", "Elliot", "Frank", "George"};
+        int[] shares = { 1000, 2500, 500, 2000, 800, 250, 400 };
+        
+        // Flag for if they are special or not
+        int[] votes = {10, 25, 0, 0, 0, 0, 4};
+    }
+}
